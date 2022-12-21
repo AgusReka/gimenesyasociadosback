3d110be2ee2b5ff8e749d53a4feb37b5704077c1
AgusReka <messicrack53@gmail.com>
2022-12-20 22:52:47 -0300

    limit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.set("pkg", pkg);
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use("/noticias", rutasNoticias);
 app.use("/usuarios", rutasUsuario);
 
