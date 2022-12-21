06f8558602a159da404d3d4588a3a8eabccbf5c8
AgusReka <messicrack53@gmail.com>
2022-12-20 22:03:56 -0300

    Arreglo De Size

--- dist/app.js
+++ dist/app.js
@@ -12,10 +12,11 @@ var _noticias = _interopRequireDefault(require("./rutas/noticias.route"));
 var _user = _interopRequireDefault(require("./rutas/user.route"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const app = (0, _express.default)();
+var bodyParser = require("body-parser");
 app.set("pkg", _package.default);
 app.use((0, _cors.default)());
 app.use((0, _morgan.default)("dev"));
-app.use(_express.default.json());
+app.use(_express.default.json({limit: '50mb'}));
 app.use("/noticias", _noticias.default);
 app.use("/usuarios", _user.default);
 app.get("/", (req, res) => {
